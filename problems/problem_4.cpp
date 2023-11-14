5476515d6cb865bb102c6c44d96bf2d53d799db5
Sohib <s.sultanov@newuu.uz>
2023-11-14 22:47:47 +0500

    1-5

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -5,7 +5,7 @@ std::string problemSolution4(const std::string &macAddress) {
     // write your code here
     if (macAddress=="FF:FF:FF:FF:FF:FF"){
         return "Broadcast";
-    }else if ((int (macAddress[1]))%2==0){
+    }else if ((int (macAddress[1] >= 'A' )) % 2== 0 && (int (macAddress[1] >= '0')) % 2 != 0){
         return "Unicast";
     } else {
         return "Multicast";
