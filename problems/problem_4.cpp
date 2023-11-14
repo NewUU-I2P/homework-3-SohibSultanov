e8f8fdaadaba4d299b571b2dbd364d7a8a9d9aae
Sohib <s.sultanov@newuu.uz>
2023-11-14 22:29:19 +0500

    1-5

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,13 @@ z#include <string>
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    if (macAddress=="FF:FF:FF:FF:FF:FF"){
+        return "Broadcast";
+    }else if ((int (macAddress[1]))%2==0){
+        return "Unicast";
+    } else {
+        return "Multicast";
+    }
     // make use of control flow statements
     // return result;
 }
